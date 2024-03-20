41f91990f7416c5ec7b4d912fef8995c1f68c491
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2024-03-20 22:03:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ let searchbox = document.querySelector(".searchbox");
 let recipe = document.querySelector(".recipe-container");
 
 const fetchRecipes = async (query) => {
-    // Construct the API URL dynamically with the query parameter
+  
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
 
-    // Clear previous search results
     recipe.innerHTML = '';
 
-    // Check if meals are found
     if (response.meals) {
         response.meals.forEach(meal => {
             const recipediv = document.createElement('div');
@@ -33,10 +31,10 @@ btn.addEventListener("click", (e) => {
     console.log("btn clicked");
     const searchinput = searchbox.value.trim();
     if (searchinput.length === 0) {
-        // If search input is empty, display a message
+       
         recipe.innerHTML = '<div class="recipe"><h3>Enter a recipe name to search</h3></div>';
     } else {
-        // If search input is not empty, fetch recipes
+     
         fetchRecipes(searchinput);
     }
 });
